3b27c561e34c9939b40e32e3223a1a28a1843e3b
Adéla Prystaszová <adelaprystaszova@gmail.com>
2025-03-11 16:54:16 +0100

    Úprava kódu na prozkoumání proměnných, přidání průvodní listiny.

--- covid-script.sql
+++ covid-script.sql
@@ -1,12 +1,11 @@
--- Vytvoření primární tabulky t_adela_prystaszova_projekt_SQL_final
--- klíče: country, date
-/*vysvětlované proměnné: 
+/* Vytvoření primární tabulky t_adela_prystaszova_projekt_SQL_final
+* klíče: country, date
+* vysvětlované proměnné: 
  * počet nově nakažených na 100 000 obyvatel
 	 * = počet nově nakažených/počet obyvatel*100 000
  * podíl pozitivních testů
 	 * = počet nově nakažených/počet testovaných
-*/
-/* vysvětlující proměnné:
+* vysvětlující proměnné:
  * 	časové
 	 * 	víkend/pracovní den (binární)
 	 * 	roční období (0–3)
@@ -22,14 +21,16 @@
 	 * průměrná denní teplota
 	 * počet hodin s nenulovými srážkami v daném dni
 	 * max. síla větru v nárazech v daném dni
- */
--- využité tabulky: countries, economies, life_expectancy, 
--- religions, covid19_basic_differences, covid19_tests, 
--- weather, lookup_table
+* využité tabulky: countries, economies, life_expectancy, 
+* religions, covid19_basic_differences, covid19_tests, 
+* weather, lookup_table
+*/
 
 
+-- 1) KONTROLA PROMĚNNÝCH
 
--- 1) kontrola proměnné country:
+-- a) proměnná country
+-- lookup_table x covid19_basic_differences
 SELECT country
 FROM covid19_basic_differences
 WHERE country NOT IN (
@@ -38,7 +39,8 @@ WHERE country NOT IN (
 	)
 GROUP BY country
 ;
--- v lookup_table nechybí žádná země z covid19_basic_differences 
+	-- v lookup_table nechybí žádná země z covid19_basic_differences 
+-- lookup_table x covid19_tests
 SELECT country
 FROM covid19_tests
 WHERE country NOT IN (
@@ -49,25 +51,24 @@ GROUP BY country
 ;
 -- v lookup_table chybí z covid19_tests:
 /*
-Czech Republic
-Democratic Republic of Congo
-Hong Kong
-Macedonia
-Myanmar
-Palestine
-South Korea
-Taiwan
-United States
+Czech Republic (v lookup_table jako Czechia)
+Democratic Republic of Congo (v lookup_table jako )
+Hong Kong (v lookup_table jako )
+Macedonia (v lookup_table jako )
+Myanmar (v lookup_table jako )
+Palestine (v lookup_table jako )
+South Korea (v lookup_table jako )
+Taiwan (v lookup_table jako )
+United States (v lookup_table jako )
 */
-
 WITH country_diff AS (
-	SELECT country
+	SELECT ISO
 	FROM covid19_tests
 	GROUP BY country)
-SELECT country
+SELECT iso3
 FROM covid19_basic_differences
 WHERE country NOT IN (
-	SELECT country
+	SELECT ISO
 	FROM country_diff)
 GROUP BY country
 ;
@@ -95,7 +96,7 @@ South Korea
 Taiwan
 United States
 */
-
+-- Nejlepší je napojit vše na lookup table
 
 -- 2) kontrola proměnné date
 	-- covid19_basic_differences: 22.1.2020–23.5.2021
